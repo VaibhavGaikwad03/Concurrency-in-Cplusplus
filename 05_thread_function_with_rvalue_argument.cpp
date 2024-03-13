60d93f8a1b7751ae3e80bcf6363910e29a19d6d5
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-03-13 19:49:21 +0530

    Add files via upload

---
+++ 05_thread_function_with_rvalue_argument.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <string>
+
+/* Thread entry point function */
+/* Requires a rvalue argument */
+void fun(std::string &&str)
+{
+    std::cout << "Ownership of \"" << str << "\" transferred to thread" << '\n';
+}
+
+int main()
+{
+    std::string str = "moveable";
+    std::cout << "Starting thread\n";
+
+    /* Wrap str in a call to std::move() */
+    std::thread t(fun, std::move(str));
+
+    t.join();
+
+    /* Verify that str has been modified */
+    std::cout << "Do you still have any data? ";
+    std::cout << (str.empty() ? "No" : "Yes") << ".\n";
+
+    return 0;
+}
