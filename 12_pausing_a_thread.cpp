fa0c7e1b7f7f66b74b7cd2a82c088a40f123823a
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-03-14 20:31:58 +0530

    Add files via upload

---
+++ 12_pausing_a_thread.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+
+using namespace std::literals;
+
+/* Task funtion */
+void hello()
+{
+    /* std::this_thread::sleep_for(std::chrono::seconds(2)); C++ 11 */
+    std::this_thread::sleep_for(2s); /* C++ 14 */
+    std::cout << "Hello, Thread!\n";
+}
+
+int main()
+{
+    /* Create an std::thread object */
+    std::cout << "Starting thread...\n";
+    std::thread t(hello);
+
+    /* Wait for the thread to complete */
+    t.join();
+
+    return 0;
+}
