0f4194f90428f5a0464d4ce94de607b616e52782
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-03-21 19:51:19 +0530

    Add files via upload

---
+++ 20_data_race.cpp
@@ -0,0 +1,26 @@
+// Unsynchronized threads which make conflicting accesses.
+// But where is the shared memory location?
+
+#include <thread>
+#include <iostream>
+
+void print(std::string str)
+{
+    // A very artificial way to display a string (hehe)
+    for (int i = 0; i < 5; i++)
+        std::cout << str[0] << str[1] << str[2] << std::endl;
+}
+
+int main()
+{
+    std::thread t1(print, "abc");
+    std::thread t2(print, "def");
+    std::thread t3(print, "xyz");
+
+    // Wait for the thread to complete
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
+}
