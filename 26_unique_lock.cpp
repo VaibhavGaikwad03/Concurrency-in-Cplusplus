446865a8e113c29b56679b07eca10b3d7fb06192
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-03-25 00:05:15 +0530

    Add files via upload

---
+++ 26_unique_lock.cpp
@@ -0,0 +1,40 @@
+// Use std::unique_lock to avoid scrambled output
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <string>
+
+using namespace std::literals;
+
+std::mutex mtx;
+
+void task(std::string str)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        // Create an std::unique_lock object
+        // This calls mtx.lock()
+        std::unique_lock<std::mutex> unq_lck(mtx);
+
+        // Start of critical section
+        std::cout << str[0] << str[1] << str[2] << std::endl;
+        // End of critical section
+
+        // Unlock the mutex
+        unq_lck.unlock();
+
+        std::this_thread::sleep_for(50ms);
+    } // Calls ~std::unique_lock
+}
+
+int main()
+{
+    std::thread t1(task, "abc");
+    std::thread t2(task, "xyz");
+    std::thread t3(task, "pqr");
+
+    t1.join();
+    t2.join();
+    t3.join();
+}
