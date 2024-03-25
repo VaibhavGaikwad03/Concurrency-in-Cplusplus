70b5c9bba4bdd0c28054dae4ea1588720e9303af
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-03-25 10:52:22 +0530

    Add files via upload

---
+++ 28_timed_mutex.cpp
@@ -0,0 +1,48 @@
+// Example of std::timed_mutex try_lock_for() member function
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std::literals;
+
+std::timed_mutex tmtx;
+
+void task1()
+{
+    std::cout << "Task1 trying to lock the mutex\n";
+    tmtx.lock();
+    std::cout << "Task1 locks the mutex\n";
+    std::this_thread::sleep_for(5s);
+    std::cout << "Task1 unlocking the mutex\n";
+    tmtx.unlock();
+}
+
+void task2()
+{
+    std::this_thread::sleep_for(500ms);
+    std::cout << "Task2 trying to lock the mutex\n";
+
+    // Try for 1 second to lock the mutex
+    while (!tmtx.try_lock_for(1s))
+        std::cout << "Task2 clould not lock the mutex\n"; // Returned false
+    // Try again on next iteration
+    // Returned true - the mutex is now locked
+
+    // Start of critical section
+    std::cout << "Task2 has locked the mutex\n";
+    // End of critical section
+
+    tmtx.unlock();
+}
+
+int main()
+{
+    std::thread t1(task1);
+    std::thread t2(task2);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
